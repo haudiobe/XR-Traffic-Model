5de5520d866913d09c3ce81247a8e8be8eb8a459
nils <nils.dvl+git@gmail.com>
2021-02-02 18:02:29 +0100

    batch scripts

--- batch.py
+++ batch.py
@@ -7,7 +7,7 @@ import json
 import argparse
 
 users = {
-    'start_frame': [ 1, 1801, 901, 2701, 451, 2251, 1351, 3251, 226, 2026, 1126, 2926, 676, 2476, 1576, 3476 ]
+    'start_frame': [ 1 ] # , 1801, 901, 2701, 451, 2251, 1351, 3251, 226, 2026, 1126, 2926, 676, 2476, 1576, 3476 ]
 }
 
 def gen_user_configs(base_cfg:dict):
@@ -50,7 +50,7 @@ class Project(object):
         cfg['S-Trace'] = str(self.get_strace_path())
         p = self.output_dir / f'{self.encode_cfg_path.stem}[{user_id}]{self.encode_cfg_path.suffix}'
         with open(p, 'w') as fp:
-            json.dump(cfg, fp)
+            json.dump(cfg, fp, indent=4)
         return p
 
     def save_ptrace_cfg(self):
@@ -59,7 +59,7 @@ class Project(object):
         cfg['P-Trace'] = str(self.get_ptrace_path())
         p = self.output_dir / self.packet_cfg_path.name
         with open(p, 'w') as fp:
-            json.dump(cfg, fp)
+            json.dump(cfg, fp, indent=4)
         return p
 
 
@@ -70,6 +70,8 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--packet_cfg', help='base packetizer config', type=str, required=True)
     parser.add_argument('-o', '--output_dir', help='output dir', type=str, required=True)
     parser.add_argument('-y', '--overwrite', help='overwrite output dir', action='store_true', required=False)
+    parser.add_argument('-S', '--plot-only-strace', help='plot only strace',  action='store_true', required=False)
+    parser.add_argument('-P', '--plot-only-ptrace', help='plot only ptrace',  action='store_true', required=False)
     
     args = parser.parse_args()
 
@@ -85,10 +87,19 @@ if __name__ == '__main__':
     P = Project(args.encode_cfg, args.packet_cfg, args.output_dir)
     packet_cfg = P.save_ptrace_cfg()
 
+    plot_only = (args.plot_only_strace or args.plot_only_ptrace)
+
     for idx, encoder_cfg in gen_user_configs(P.encode_cfg):
         encoder_cfg_path = P.save_strace_cfg(idx, encoder_cfg)
-        subprocess.run(['python3', './xrtm_encoder.py', '-c', str(encoder_cfg_path), '-u', str(idx)])
-        subprocess.run(['python3', './xrtm_packetizer.py', '-c', str(packet_cfg), '-u', str(idx)])
-        subprocess.run(['python3', './plot.py', '-s', P.get_strace_path(idx), '-n'])
-        subprocess.run(['python3', './plot.py', '-p', P.get_ptrace_path(idx), '-n'])
+        if not plot_only:
+            cmd = ['python3', './xrtm_encoder.py', '-c', str(encoder_cfg_path), '-u', str(idx)]
+            print(cmd)
+            subprocess.run(['python3', './xrtm_encoder.py', '-c', str(encoder_cfg_path), '-u', str(idx)])
+            subprocess.run(['python3', './xrtm_packetizer.py', '-c', str(packet_cfg), '-u', str(idx)])
+        if args.plot_only_strace or (not plot_only):
+            subprocess.run(['python3', './plot.py', '-s', P.get_strace_path(idx), '-n'])
+        if args.plot_only_ptrace or (not plot_only):
+            subprocess.run(['python3', './plot.py', '-p', P.get_ptrace_path(idx), '-n'])
+        
+
         
